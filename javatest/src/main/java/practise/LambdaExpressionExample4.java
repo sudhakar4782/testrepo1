8820aea06fb65183249972331cd239582d8d28b4
sudhakar4782 <sudhakar4782@gmail.com>
2021-01-25 18:08:13 +0530

    new project

---
+++ javatest/src/main/java/practise/LambdaExpressionExample4.java
@@ -0,0 +1,22 @@
+package practise;
+
+interface Sayable2{  
+    public String say(String name);  
+}  
+  
+public class LambdaExpressionExample4{  
+    public static void main(String[] args) {  
+      
+        // Lambda expression with single parameter.  
+    	Sayable2 s1=(name)->{  
+            return "Hello, "+name;  
+        };  
+        System.out.println(s1.say("Sonoo"));  
+          
+        // You can omit function parentheses    
+        Sayable2 s2= name ->{  
+            return "Hello, "+name;  
+        };  
+        System.out.println(s2.say("Sonoo"));  
+    }  
+} 
